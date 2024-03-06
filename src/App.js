4ec4a4f9cf784f9ba74ab57d0db1f442c5bbfb5d
“Nathan-McDonald” <nm00028@mix.wvu.edu>
2024-03-06 02:33:47 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
+      <h1>
+        CS 230L
+      </h1>
+      <h2>
+        Section 002
+      </h2>
+      <p>
+        WVU ID: 800368871
+      </p>
+      <p>
+        Hi, I am Nathan McDonald
+      </p>
     </div>
   );
 }
